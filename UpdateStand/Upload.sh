34015eb988ad9135b5ed1239c51d5d2ee265dba9
Liam <liamkloppers96@gmail.com>
2021-06-10 17:00:37 +0200

    Update config locations

--- UpdateStand/Upload.sh
+++ UpdateStand/Upload.sh
@@ -13,6 +13,6 @@ for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
         device="/dev/$devname - $ID_SERIAL" #| grep Silicon_Labs_CP2104_USB | cut -d- -f1 
 	espPort=$(echo "$device" | grep Silicon_Labs_CP2104_USB | cut -d " " -f1)
 	[[ -z "$espPort" ]] && exit
-	python -m esptool --chip esp32 --port "$espPort" --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ./boot_app0.bin 0x1000 ./bootloader_qio_80m.bin 0x10000 ./ESP_REST_Sketch.ino.bin 0x8000 ./ESP_REST_Sketch.ino.partitions.bin 
+	~/.local/bin/esptool.py  --chip esp32 --port "$espPort" --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ./boot_app0.bin 0x1000 ./bootloader_qio_80m.bin 0x10000 ./ESP_REST_Sketch.ino.bin 0x8000 ./ESP_REST_Sketch.ino.partitions.bin 
     )
 done
