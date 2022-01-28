61cc41791b9856add168741dbcbe7b95826890c4
liam <liam@madebyeden.com>
2022-01-28 15:23:24 +0200

    Feat: Pair from ssid config

--- PairStand.sh
+++ PairStand.sh
@@ -5,33 +5,37 @@ stationConfig=/home/eden/StationConfig/StationConfig.json
 pair_ssid="$( jq -r '.AP_Wifi.SSID' "$stationConfig" )"
 pair_pass="$( jq -r '.AP_Wifi.Password' "$stationConfig" )"
 
-SSID="SSID:""$pair_ssid"
-PASS="Pass:""$pair_pass"
-echo $SSID
+pair_ssid="$pair_ssid"
+pair_pass="$pair_pass"
+#debug
+echo $pair_ssid
 
 sleep 4
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
     (
         syspath="${sysdevpath%/dev}"
         devname="$(udevadm info -q name -p $syspath)"
-	#echo $devname
+        #echo $devname
         [[ "$devname" == "bus/"* ]] && exit
         eval "$(udevadm info -q property --export -p $syspath)"
         [[ -z "$ID_SERIAL" ]] && exit
-        device="/dev/$devname - $ID_SERIAL" #| grep Silicon_Labs_CP2104_USB | cut -d- -f1 
-	espPort=$(echo "$device" | grep Silicon_Labs_CP2104_USB | cut -d " " -f1)
-	#echo $device
-	[[ -z "$espPort" ]] && exit
-	stty -F "$espPort" raw -echo 115200
+        device="/dev/$devname - $ID_SERIAL" 
 
-	echo -e "$SSID"> $espPort
+        #Look for serial converter matching stand by manufacturer
+        ##Bus 001 Device 009: ID 1a86:55d4 QinHeng Electronics 
+        espPort=$(echo "$device" | grep "1a86_USB_Single_Serial" | cut -d " " -f1)
 
-	sleep 1
+        [[ -z "$espPort" ]] && exit
+        #open the port
+        stty -F "$espPort" raw -echo 115200 cs8 cread clocal
+        #sleep to prevent garbage being sent during port opening
+        sleep 2
 
-	echo -e "$PASS"> $espPort
- 
+        #echo to port and do not ouput any trailing new lines
+        #escape characters are used o/*n the stand to side to process ";"
+        echo -e "wifi;""$pair_ssid"";""$pair_pass"> $espPort
+
     )
 done
 
-#echo "$pair_ssid" >> /home/sixshooter/Documents/Eden/Code/Snacker/StandPairing/RAN.txt
 
